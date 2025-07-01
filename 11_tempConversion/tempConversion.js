1775140713a25e632e709190f64a19e7e3d9a9bd
dprings7 <davepringle07@gmail.com>
2025-07-01 22:21:42 +0100

    Complete exercise using README specs

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+    converted = (fahrenheit - 32) * (5/9);
+    return Number(converted.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+    converted = (celsius * (9/5)) + 32;  
+    return Number(converted.toFixed(1));
 };
 
 // Do not edit below this line
