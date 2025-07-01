d16c7c804c4a948c537ebe0c7e75add1b02f3a36
dprings7 <davepringle07@gmail.com>
2025-07-01 07:45:57 +0100

    Complete exercise using README specs

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, repeat) {
 
+    if (repeat < 0) {
+        return 'ERROR';
+    }
+    concatenatedStr = ''
+    for (i = 0; i < repeat; i++) {
+        concatenatedStr += str;
+    }
+
+    return concatenatedStr;
 };
 
 // Do not edit below this line
