20bc962ad0ea2cac4ec1187990e7709d3026b76a
dprings7 <davepringle07@gmail.com>
2025-07-01 21:51:50 +0100

    Complete exercise using README spec

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    sum = 0;
 
+    if ((a < 0 || b < 0) || (!Number.isInteger(a) || !Number.isInteger(b))) {
+        return 'ERROR';
+    }
+
+    //always start from smallest number
+    if (a > b) {
+        let c = b;
+        b = a;
+        a = c;
+    }
+
+    for (let i = a; i <= b; i++) {sum += i;}
+
+    return sum;
 };
 
 // Do not edit below this line
