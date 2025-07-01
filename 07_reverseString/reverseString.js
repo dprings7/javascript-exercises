667edb07a02a91dd13db05e15017684a2933114c
dprings7 <davepringle07@gmail.com>
2025-07-01 08:03:18 +0100

    Complete exercise using README specs

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    splitWord = word.split('');
+    reversedWord = '';
 
+    for (i = splitWord.length - 1; i >= 0; i--) {
+        reversedWord += splitWord[i];
+    }
+
+    return reversedWord;
 };
 
 // Do not edit below this line
