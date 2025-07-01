9aacae6510192ee1ff795448c3483194090514c5
dprings7 <davepringle07@gmail.com>
2025-07-01 08:53:41 +0100

    Complete exercise using README specs

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    const args = Array.prototype.slice.call(arguments, 1);
+    for (arg of args) {
+        while (arr.includes(arg)) {
+            index = arr.indexOf(arg)
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
