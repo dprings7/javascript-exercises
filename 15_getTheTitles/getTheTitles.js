b7019c890ebd473e2069d76417d0d1516581a410
dprings7 <davepringle07@gmail.com>
2025-07-13 13:44:50 +0100

    Complete exercise using README specs

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let titles = []
+    
+    for (const element of arr) {
+        titles.push(Object.values(element)[0]);
+    }
+    
+    return titles;
 };
 
 // Do not edit below this line
