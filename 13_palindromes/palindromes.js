e0bf84730ba7b08b976a6c5714ae08f62177a5ed
dprings7 <davepringle07@gmail.com>
2025-07-13 09:06:11 +0100

    Complete exercise using README specs

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let palindrome = true;
+    // Normalise
+    str = str.toLowerCase();
+    // Remove any non letters
+    let arr = Array.from(str);
+    filtered = arr.filter((char) => isAlphaNumeric(char))
+    
+    // go through array and check if start char = end char
+    for (let i = 0; i < filtered.length; i++) {
+        if (filtered[i] !== (filtered[(filtered.length-1 )- i])) {
+            palindrome = false
+            break;
+        }
+    }
+    return palindrome
 };
+
+function isAlphaNumeric(char) {
+    return /^[a-z0-9]+$/.test(char);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
