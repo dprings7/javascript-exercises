0e15d57b894d7244882590ce4718e1c78ae48def
dprings7 <davepringle07@gmail.com>
2025-07-13 14:47:13 +0100

    Complete exercise using README specs

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const date = new Date();
+    let ages = arr.map((person) => {
+        if ('yearOfDeath' in person) {
+            person['age'] = person.yearOfDeath - person.yearOfBirth;
+            }
+        else {
+            person['age'] = date.getFullYear() - person.yearOfBirth;
+        }
+        return person;
+    })
+    .sort((a, b) => b.age - a.age);
 
+    return ages[0];
+
+    let sorted = ages.sort((a, b) => b.age - a.age);
+    return sorted[0];
 };
 
 // Do not edit below this line
