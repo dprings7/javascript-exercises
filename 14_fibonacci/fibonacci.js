0ab1d04ab7c2a0fbbdc1681df9090e48b32ba8d3
dprings7 <davepringle07@gmail.com>
2025-07-13 13:23:40 +0100

    Complete exercise using README specs

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(val) {
+    // Convert value if string
+    val = +val;
+    // Handle edge cases
+    if (val < 0) return "OOPS";
+    if (val <= 1) return val;
 
+    let prev = 0;
+    let curr = 1;
+    while (val > 1) {
+        let temp = curr
+        curr = prev + curr
+        prev = temp;
+        val--;
+    }
+    return curr;
 };
 
 // Do not edit below this line
